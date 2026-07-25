cfd14cb1f6171e00432d94788b50ae3a33fff7b5
Baghdaddy27 <elbrigadista@pm.me>
2026-07-25 12:06:12 +0200

    update1

--- Config/start_dayz_takistan.sh
+++ Config/start_dayz_takistan.sh
@@ -3,6 +3,6 @@
 cd /home/jordan/Games/DayZ_Server
 
 # Start server
-./DayZServer -config=server_takistan.cfg -port=2301 "-mod=@CF;@Dabs Framework;@TakistanPlus;@DayZ-Expansion-Licensed;@DayZ-Expansion-Bundle;@Community-Online-Tools;@Takistan Clothing" "-serverMod=@DayZ-Dynamic-AI-Addon" -BEpath=battleye -profiles=profiles_takistan -dologs -adminlog -netlog -freezecheck
+./DayZServer -config=server_takistan.cfg -port=2301 "-mod=@CF;@Dabs Framework;@TakistanPlus;@DayZ-Expansion-Licensed;@DayZ-Expansion-Bundle;@Community-Online-Tools;@Takistan Clothing;@PVEZ Reloaded" "-serverMod=@DayZ-Dynamic-AI-Addon" -BEpath=battleye -profiles=profiles_takistan -dologs -adminlog -netlog -freezecheck
 
 read -p "Done. Press Enter to close..."
