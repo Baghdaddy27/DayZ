b4668bd3275c297a57ecf11fbac3c98535c2e57e
Baghdaddy27 <elbrigadista@pm.me>
2026-08-17 14:08:49 +0200

    removed redundant files

--- Config/server_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/jordan/Games/DayZ_Server
-
-steamcmd +force_install_dir /home/jordan/Games/DayZ_Server +login kriegergaming +app_update 223350 +workshop_download_item 221100 1559212036 +workshop_download_item 221100 1564026768 +workshop_download_item 221100 2116157322 +workshop_download_item 221100 2545327648 +workshop_download_item 221100 2572331007 +workshop_download_item 221100 2874589934 +quit
-
-read -p "Done. Press Enter to close..."
